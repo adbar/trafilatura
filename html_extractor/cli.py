748f583e893360dec18b25949812556755fad2c5
Adrien Barbaresi <barbaresi@bbaw.de>
2019-05-09 14:29:01 +0200

    download via CLI

--- html_extractor/cli.py
+++ html_extractor/cli.py
@@ -10,14 +10,23 @@ import argparse
 import logging
 import sys
 
+import requests
+
 from html_extractor import process_record
 
+
+def fetch_url(url):
+    """ Fetch page using requests """
+    response = requests.get(url)
+    if int(response.status_code) != 200:
+        return None
+    return response.text
 
 
 def examine(htmlstring):
     """ Generic safeguards and triggers """
     # safety check
-    if len(htmlstring) > 20000000:
+    if len(htmlstring) > 10000000:
         sys.stderr.write('# ERROR: file too large\n')
     elif len(htmlstring) < 10:
         sys.stderr.write('# ERROR: file too small\n')
@@ -28,28 +37,36 @@ def examine(htmlstring):
     return None
 
 
-
 def main():
     """ Run as a command-line utility. """
     # arguments
     argsparser = argparse.ArgumentParser()
+    argsparser.add_argument("-u", "--URL", help="custom URL download")
     argsparser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = argsparser.parse_args()
 
     if args.verbose:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-    ## process input on STDIN
-    # unicode check
-    try:
-        htmlstring = sys.stdin.read()
-    except UnicodeDecodeError as err:
-        sys.stderr.write('# ERROR system/buffer encoding: ' + str(err) + '\n')
-        sys.exit(1)
+    # process input URL
+    if args.URL:
+        htmlstring = fetch_url(args.URL)
+        if htmlstring is None:
+            sys.stderr.write('# ERROR no valid result for url: ' + args.URL + '\n')
+            sys.exit(1)
+    # process input on STDIN
+    else:
+        # unicode check
+        try:
+            htmlstring = sys.stdin.read()
+        except UnicodeDecodeError as err:
+            sys.stderr.write('# ERROR system/buffer encoding: ' + str(err) + '\n')
+            sys.exit(1)
 
     result = examine(htmlstring)
     if result is not None:
         sys.stdout.write(result + '\n')
+
 
 if __name__ == '__main__':
     main()
