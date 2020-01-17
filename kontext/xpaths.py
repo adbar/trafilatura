8807f9afe668c3f1f9fe40af8a19e8409a0ac9b4
Adrien Barbaresi <barbaresi@bbaw.de>
2020-01-17 18:59:14 +0100

    xpaths tweaked

--- kontext/xpaths.py
+++ kontext/xpaths.py
@@ -7,53 +7,54 @@ File containing XPath expressions to extract metadata.
 
 
 author_xpaths = [
-'//a[@rel="author"]',
-'//a[@class="author"]',
-'//span[@class="author"]',
-'//address[@class="author"]',
+'//a[@rel="author" or @class="author" or rel="me"]',
+'//*[@rel="author" or @class="author" or rel="me"]',
 '//author',
+'//a[contains(@class, "author")]',
 ]
-# rel="me"
+# json author
+# '//address[@class="author"]',
+# screenname
+# @class, 'byline'
+
 
 categories_xpaths = [
-"//div[starts-with(@class, 'post-info')]//a",
-"//div[starts-with(@class, 'postinfo')]//a",
-"//div[starts-with(@class, 'post-meta')]//a",
+"""//div[starts-with(@class, 'post-info') or starts-with(@class, 'postinfo') or
+    starts-with(@class, 'post-meta') or starts-with(@class, 'postmeta') or
+    starts-with(@class, 'meta')]//a""",
+
 '//div[@class="row"]//a',
-'//div[@class="byline"]//a',
-"//div[starts-with(@class, 'postmeta')]//a",
-"//div[starts-with(@class, 'entry-meta')]//a",
-"//div[starts-with(@class, 'entry-info')]//a",
-"//div[starts-with(@class, 'entry-utility')]//a",
+"""//div[starts-with(@class, 'entry-meta') or starts-with(@class, 'entry-info') or
+    starts-with(@class, 'entry-utility')]//a""",
 "//div[starts-with(@id, 'postpath')]//a",
-"//p[starts-with(@class, 'postmeta')]//a",
-"//p[starts-with(@class, 'entry-categories')]//a",
-'//p[@class="postinfo"]//a',
-'//p[@id="filedunder"]//a',
-'//p[@class="postdate"]//a',
-"//footer[starts-with(@class, 'entry-meta')]//a",
-"//footer[starts-with(@class, 'entry-footer')]//a",
+"//p[starts-with(@class, 'postmeta') or starts-with(@class, 'entry-categories')]//a",
+'//p[@class="postinfo" or @id="filedunder"]//a',
+"//footer[starts-with(@class, 'entry-meta') or starts-with(@class, 'entry-footer')]//a",
 '//li[@class="post-category"]//a',
 '//span[@class="postcategory"]//a',
 '//span[@class="entry-category"]//a',
 '//header[@class="entry-header"]//a',
 '//div[@class="tags"]//a',
-"//div[starts-with(@class, 'meta')]//a",
 ]
+# "//div[contains(@class, 'byline')]",
+# "//p[contains(@class, 'byline')]",
+
 
 tags_xpaths = [
+'//div[@class="tags"]//a',
 "//p[starts-with(@class, 'entry-tags')]//a",
-'//div[@class="row"]//a',
-'//div[@class="jp-relatedposts"]//a',
-"//div[starts-with(@class, 'tag')]//a",
-'//div[@class="entry-utility"]//a',
-"//div[starts-with(@class, 'meta')]//a",
+'''//div[@class="row" or @class="jp-relatedposts" or @class="entry-utility" or
+    starts-with(@class, 'tag') or starts-with(@class, 'postmeta') or
+    starts-with(@class, 'meta')]//a''',
 '//*[@class="entry-meta"]//a'
 ]
-# , "//div[starts-with(@class, 'postmeta')]//a"
+
 
 title_xpaths = [
 '//h1',
+'//h1[contains(@class, "title") or contains(@id, "title")]',
+'//*[contains(@class, "post-title") or contains(@class, "entry-title")]',
 '//head/title',
 '//h2',
 ]
+# json headline
