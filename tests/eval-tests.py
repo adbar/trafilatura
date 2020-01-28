11a6f65fa7e294ab4dce6cadefb03b6e703f6618
Adrien Barbaresi <barbaresi@bbaw.de>
2020-01-28 14:40:37 +0100

    add evaluation data

---
+++ tests/eval-tests.py
@@ -0,0 +1,65 @@
+EVAL_PAGES = {
+'https://www.ahlen.de/start/aktuelles/aktuelle/information/nachricht/aus-ahlen/reparaturcafe-am-31-januar/': {
+    'file': '',  # stays empty until download
+    'author': '',
+    'title': '',
+    'date': '',
+    'description': '',  # in HTML source
+    'categories': [''],
+    'tags': [''],
+    'with': [''],  # 3 segments
+    'without': [''],  # 3 segments
+    'comments': [''],  # 0 or 3 segments
+    'license': '',  # if CC-...
+    'region': '',  # if obvious: DE, CH, AT
+},
+
+https://www.travanto.de/ferienhaus/lierfeld/40222/ferienhaus-feinen.php
+
+https://rete-mirabile.net/notizen/15-jahre-rete-mirabile/
+
+https://shop.nmb-media.de/eBay-Template-Datenschutz-Google-Fonts-Fontawesome
+
+https://viertausendhertz.de/ddg48/
+
+http://www.bibliothek2null.de/2014/05/18/alles-neue-mach-der-mai/
+
+http://www.helge.at/2014/03/warum-wien-zu-blod-fur-eine-staufreie-mahu-ist/
+
+http://www.heiko-adams.de/laufen-im-winter-von-baeh-zu-yeah-in-12-monaten/
+
+http://www.nalas-loewenseiten.info/loewen-lexikon/?letter=M
+
+https://blogoff.de/2015/11/12/i-htm/
+
+https://de.globalvoices.org/2019/04/30/ein-jahr-voller-proteste-nicaraguaner-wollen-nicht-mehr-nur-den-rucktritt-ortegas-sondern-einen-neuanfang/
+
+http://wir-empfehlen.info/?p=3289
+
+https://www.reisen-mit-dem-internet.de/europa/deutschland/niedersachsen/ostfriesland/emden-21416
+
+https://nextkabinett.wordpress.com/2014/01/17/derek-jarman-%c2%b7-the-garden/
+
+https://sprechblase.wordpress.com/2019/11/17/elektro-zapfsaeulen/
+
+https://creeny.wordpress.com/2020/01/24/nebelsuppe-6/
+
+https://nurmeinstandpunkt.wordpress.com/2020/01/23/blogposting-01-23-2020/
+
+https://flowfx.de/blog/copy-paste-from-tmux-to-system-clipboard/
+
+https://wiki.piratenpartei.de/HE:Kassel/Stammtisch
+
+https://gnadlib.wordpress.com/2020/01/05/scherenschnitt-3/#more-27529
+
+http://www.buero-hoppe.de/baumgutachten.htm
+
+https://www.spontis.de/schwarze-szene/liebe-leser-bitte-rutschen-sie-nicht-in-das-neue-jahrzehnt/
+
+https://www.schneems.com/2018/10/09/pair-with-me-rubocop-cop-that-detects-duplicate-array-allocations/
+
+https://hackernoon.com/how-to-scrape-google-with-python-bo7d2tal
+
+
+
+}
