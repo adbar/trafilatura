a79c7a1cd3e9c4b78c619696cd2bd55d1d704b39
Adrien Barbaresi <barbaresi@bbaw.de>
2020-09-22 12:58:56 +0200

    settings update

--- trafilatura/settings.py
+++ trafilatura/settings.py
@@ -10,39 +10,36 @@ from multiprocessing import cpu_count
 
 from lxml.html.clean import Cleaner
 
+
+# sanity checks
 MAX_FILE_SIZE = 20000000
 MIN_FILE_SIZE = 10
 SLEEP_TIME = 2
-
 DOWNLOAD_THREADS = 5
 
-# USER_AGENT = 'trafilatura/0.5.0 (+https://github.com/adbar/trafilatura)'
 
-## extract
+# extraction config
 MIN_EXTRACTED_SIZE = 200
 MIN_EXTRACTED_COMM_SIZE = 10
 MIN_DUPLCHECK_SIZE = 100
 MIN_OUTPUT_SIZE = 25
 MIN_OUTPUT_COMM_SIZE = 10
 MAX_OUTPUT_TREE_LENGTH = 500
-PROCESSING_TIMEOUT = 30
+
+
+# file output
 MAX_FILES_PER_DIRECTORY = 1000
-
+FILENAME_LEN = 8
 FILE_PROCESSING_CORES = min(cpu_count(), 16)  # 16 processes at most
+PROCESSING_TIMEOUT = 30
 
+
+# deduplication
 LRU_SIZE = 65536
 MAX_REPETITIONS = 2
 
-FILENAME_LEN = 8
-
 
 # filters
-DETECTION_LANGUAGES = ['af', 'am', 'an', 'ar', 'as', 'az', 'be', 'bg', 'bn', 'br', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'dz', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fr', 'ga', 'gl', 'gu', 'he', 'hi', 'hr', 'ht', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'jv', 'ka', 'kk', 'km', 'kn', 'ko', 'ku', 'ky', 'la', 'lb', 'lo', 'lt', 'lv', 'mg', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'nb', 'ne', 'nl', 'nn', 'no', 'oc', 'or', 'pa', 'pl', 'ps', 'pt', 'qu', 'ro', 'ru', 'rw', 'se', 'si', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'ug', 'uk', 'ur', 'vi', 'vo', 'wa', 'xh', 'zh', 'zu']
-# less languages = smaller footprint
-# DETECTION_LANGUAGES = ['de', 'en', 'es', 'fr', 'ja', 'nl', 'ru']
-
-#CORPUS_VERSION = 2017.1
-
 CUT_EMPTY_ELEMS = {'article', 'b', 'blockquote', 'dd', 'div', 'dt', 'em',
                    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'li', 'main',
                    'p', 'pre', 'q', 'section', 'span', 'strong'}
@@ -53,9 +50,9 @@ MANUALLY_CLEANED = ['audio', 'aside', 'canvas', 'figure', 'footer', 'form', 'hea
                     # secondary
                     'applet', 'area', 'blink', 'button', 'datalist', 'details', 'dialog', 'embed',
                     'fieldset', 'iframe', 'link', 'input', 'label', 'legend',
-                    'marquee', 'math', 'menu', 'nav', 'noscript', 'option',
-                    'rp', 'rt', 'rtc', 'source', 'style', 'track', 'textarea', 'time']
-                    # 'frame', 'frameset'
+                    'marquee', 'math', 'menu', 'menuitem', 'nav', 'noscript', 'option',
+                    'rp', 'rt', 'rtc', 'select', 'source', 'style', 'track', 'textarea', 'time', 'use']
+                    # 'frame', 'frameset', 'meta', 'hr', 'ins'
 
 MANUALLY_STRIPPED = ['abbr', 'acronym', 'address', 'bdi', 'bdo', 'big', 'cite', 'data', 'font', 'hgroup', 'img', 'ins', 'mark', 'meta', 'ruby', 'small']
 
@@ -82,6 +79,8 @@ HTML_CLEANER.kill_tags = MANUALLY_CLEANED
 
 TAG_CATALOG = frozenset(['blockquote', 'code', 'del', 'fw', 'head', 'hi', 'lb', 'list', 'p', 'pre', 'q', 'quote'])
 # 'span', 'item'
+
+# USER_AGENT = 'trafilatura/0.5.0 (+https://github.com/adbar/trafilatura)'
 
 # JUSTEXT_DEFAULT = 'German'
 # JT_STOPLIST = None  # could be a list
