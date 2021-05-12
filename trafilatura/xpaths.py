8afd125cbef23ccbbdf93356bc75b6146b8e7cdb
Adrien Barbaresi <barbaresi@bbaw.de>
2021-05-12 20:05:07 +0200

    extended XPath expressions

--- trafilatura/xpaths.py
+++ trafilatura/xpaths.py
@@ -16,7 +16,7 @@ BODY_XPATH = [
     contains(@class, "post-body") or contains(@class, "post-entry") or contains(@class, "postentry") or
     contains(@class, "post-content") or contains(@class, "post_content") or
     contains(@class, "postcontent") or contains(@class, "postContent") or
-    contains(@class, "article-text") or contains(@class, "articletext") or contains(@class, "articleText")]''',
+    contains(@class, "article-text") or contains(@class, "articletext") or contains(@class, "articleText") or contains(@class, "field-body")]''',
     '''.//*[(self::article or self::div or self::main or self::section)][contains(@id, "entry-content") or
     contains(@class, "entry-content") or contains(@id, "article-content") or
     contains(@class, "article-content") or contains(@id, "article__content") or
@@ -34,7 +34,7 @@ BODY_XPATH = [
     contains(@class, 'section-content') or contains(@class, 'single-content') or
     contains(@class, 'single-post') or
     contains(@class, 'main-column') or contains(@class, 'wpb_text_column') or
-    starts-with(@id, 'primary') or @class="text" or
+    starts-with(@id, 'primary') or starts-with(@class, 'article ') or @class="text" or
     @class="cell" or @id="story" or @class="story" or
     contains(translate(@class, "ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz"), "fulltext")]""",
     '''.//*[(self::article or self::div or self::main or self::section)][contains(@id, "main-content") or
@@ -55,9 +55,9 @@ BODY_XPATH = [
 
 
 COMMENTS_XPATH = [
-    """.//*[(self::div or self::list or self::section)][contains(@id, 'commentlist')
-    or contains(@class, 'commentlist')  or contains(@class, 'comment-page') or
-    contains(@class, 'comment-list') or contains(@class, 'comments-list') or
+    """.//*[(self::div or self::section or self::list)][contains(@id, 'commentlist')
+    or contains(@class, 'commentlist') or contains(@class, 'comment-page') or
+    contains(@id, 'comment-list') or contains(@class, 'comments-list') or
     contains(@class, 'comments-content')]""",
     """.//*[(self::div or self::section or self::list)][starts-with(@id, 'comments')
     or starts-with(@class, 'comments') or starts-with(@class, 'Comments') or
@@ -70,20 +70,40 @@ COMMENTS_XPATH = [
 # or contains(@class, 'Comments')
 
 
+REMOVE_COMMENTS_XPATH = [
+    """.//*[(self::div or self::section)][@id='comments' or @class='comments' or
+    contains(@id, 'commentlist') or contains(@class, 'commentlist')
+    or contains(@id, 'comment-list') or contains(@class, 'comments-list') or
+    starts-with(@id, 'comments')
+    or starts-with(@class, 'comments') or starts-with(@class, 'Comments')
+    ]""",
+]
+# or self::list
+# or contains(@id, 'commentlist') or contains(@class, 'commentlist') or
+#    contains(@id, 'comment-list') or contains(@class, 'comments-list') or
+#    contains(@class, 'comments-content')
+#  or starts-with(@id, 'comment-') or starts-with(@class, 'comment-') or
+#    contains(@class, 'article-comments')
+#  or contains(@class, 'comment-page') or
+
+
 DISCARD_XPATH = [
     '''.//*[contains(@id, "footer") or contains(@class, "footer") or
     contains(@id, "bottom") or contains(@class, "bottom")]''',
     # related posts, sharing jp-post-flair jp-relatedposts, news outlets + navigation
-    # or self::article
-    '''.//*[(self::div or self::item or self::list or self::p or self::section or self::span)][
+    #'''.//*[
+    # self::article or self::link
+    '''.//*[(self::div or self::item or self::list
+             or self::p or self::section or self::span)][
     contains(@id, "related") or contains(translate(@class, "R","r"), "related") or
     contains(@id, "viral") or contains(@class, "viral") or
     starts-with(@id, "shar") or starts-with(@class, "shar") or contains(@class, "share-") or
     contains(@id, "social") or contains(@class, "social") or contains(@class, "sociable") or
     contains(@id, "syndication") or contains(@class, "syndication") or
-    starts-with(@id, "jp-") or starts-with(@id, "dpsp-content")
-    or
-    contains(@id, "teaser") or contains(translate(@class, "T","t"), "teaser")
+    starts-with(@id, "jp-") or starts-with(@id, "dpsp-content") or
+    contains(@class, "embedded") or contains(@class, "embed")
+    or contains(@id, "teaser") or contains(@class, "teaser") or
+    contains(translate(@class, "T","t"), "teaser")
     or contains(@id, "newsletter") or contains(@class, "newsletter") or
     contains(@id, "cookie") or contains(@class, "cookie") or contains(@id, "tags")
     or contains(@class, "tags")  or contains(@id, "sidebar") or
@@ -92,7 +112,7 @@ DISCARD_XPATH = [
     contains(@id, "menu") or contains(@class, "menu") or
     starts-with(@id, "nav") or starts-with(@class, "nav") or
     contains(@id, "navigation") or contains(translate(@class, "N","n"), "navigation")
-    or contains(@role, "navigation")
+    or contains(@role, "navigation") or contains(@class, "navbar")
     or contains(@class, "navbox") or starts-with(@class, "post-nav")
     or contains(@id, "breadcrumb") or contains(@class, "breadcrumb") or
     contains(@id, "bread-crumb") or contains(@class, "bread-crumb") or
@@ -105,7 +125,8 @@ DISCARD_XPATH = [
     contains(@class, "user-info") or contains(@class, "user-profile") or
     contains(@class, "-ad-") or contains(@class, "-icon")
     or contains(@class, "article-infos") or
-    contains(translate(@class, "I","i"), "infoline")]''',
+    contains(translate(@class, "I","i"), "infoline")
+    or contains(@class, "options")]''',
     # comment debris
     '''.//*[@class="comments-title" or contains(@class, "comments-title") or contains(@class, "nocomments") or starts-with(@id, "reply-") or starts-with(@class, "reply-") or
     contains(@class, "-reply-") or contains(@class, "message") or contains(@id, "akismet") or contains(@class, "akismet")]''',
@@ -117,7 +138,7 @@ DISCARD_XPATH = [
 # .//header # contains(@id, "header") or contains(@class, "header") or
 # contains(@id, "link") or contains(@class, "link")
 # class contains cats
-# or contains(@class, "hidden ")
+# or contains(@class, "hidden ")  or contains(@class, "-hide")
 
 
 COMMENTS_DISCARD_XPATH = [
