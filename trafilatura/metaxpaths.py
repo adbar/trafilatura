b09e60abe396abaa2cef4f62484925c2ecabd02c
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2021-07-15 19:37:03 +1000

    Fixed camel/title/uppercase tags names on metaxpaths

--- trafilatura/metaxpaths.py
+++ trafilatura/metaxpaths.py
@@ -5,25 +5,39 @@ File containing XPath expressions to extract metadata.
 # code available from https://github.com/adbar/trafilatura/
 # under GNU GPLv3+ license
 
+attr_types = {
+    'class': 'translate(@class,"ABCDEFGHIJKLMNOPURSTUWXYZ","abcdefghijklmnopurstuwxyz")',
+    'id': 'translate(@id,"ABCDEFGHIJKLMNOPURSTUWXYZ","abcdefghijklmnopurstuwxyz")',
+    'itemprop': 'translate(@itemprop,"ABCDEFGHIJKLMNOPURSTUWXYZ","abcdefghijklmnopurstuwxyz")',
+    'rel': 'translate(@rel,"ABCDEFGHIJKLMNOPURSTUWXYZ","abcdefghijklmnopurstuwxyz")',
+}
 
 author_xpaths = [
-    '//*[(self::a or self::address or self::link or self::p or self::span)][@rel="author" or @id="author" or @class="author" or rel="me"]|//author',
-    '//*[(self::a or self::span)][contains(@class, "authors") or contains(@class, "author") or contains(@class, "posted-by") or contains(@itemprop, "author")]',
-    '//*[(self::a or self::div or self::p or self::span)][contains(@class, "byline")]',
-    '//*[contains(@class, "author") or contains(@class, "screenname")]',
+    '//*[(self::a or self::address or self::link or self::p or self::span)][' + attr_types['rel'] + '="author" or ' +
+    attr_types['id'] + '="author" or ' + attr_types['class'] + '="author" or ' + attr_types['rel'] + '="me"]|//author',
+    '//*[(self::a or self::span)][contains(' + attr_types['class'] + ', "authors") or (contains(' + attr_types[
+        'class'] + ', "author") and not(contains(' + attr_types['class'] + ', "prefix"))) or contains(' + attr_types[
+        'class'] + ', "posted-by") or contains(' + attr_types['itemprop'] + ', "author")]',
+    '//*[(self::a or self::div or self::p or self::span)][contains(' + attr_types[
+        'class'] + ', "byline") or contains(' + attr_types['class'] + ', "journalist")]',
+    '//*[contains(' + attr_types['class'] + ', "author") or contains(' + attr_types['class'] + ', "screenname")]',
 ]
 
-
 categories_xpaths = [
-    """//div[starts-with(@class, 'post-info') or starts-with(@class, 'postinfo') or
-    starts-with(@class, 'post-meta') or starts-with(@class, 'postmeta') or
-    starts-with(@class, 'meta') or starts-with(@class, 'entry-meta') or starts-with(@class, 'entry-info') or
-    starts-with(@class, 'entry-utility') or starts-with(@id, 'postpath')]//a""",
-    "//p[starts-with(@class, 'postmeta') or starts-with(@class, 'entry-categories') or @class='postinfo' or @id='filedunder']//a",
-    "//footer[starts-with(@class, 'entry-meta') or starts-with(@class, 'entry-footer')]//a",
-    '//*[(self::li or self::span)][@class="post-category" or @class="postcategory" or @class="entry-category"]//a',
-    '//header[@class="entry-header"]//a',
-    '//div[@class="row" or @class="tags"]//a',
+    '//div[starts-with(' + attr_types['class'] + ', "post-info") or starts-with(' + attr_types[
+        'class'] + ', "postinfo") or starts-with(' + attr_types['class'] + ', "post-meta") or starts-with(' +
+    attr_types['class'] + ', "postmeta") or starts-with(' + attr_types['class'] + ', "meta") or starts-with(' +
+    attr_types['class'] + ', "entry-meta") or starts-with(' + attr_types['class'] + ', "entry-info") or starts-with(' +
+    attr_types['class'] + ', "entry-utility") or starts-with(' + attr_types['id'] + ', "postpath")]//a',
+    '//p[starts-with(' + attr_types['class'] + ', "postmeta") or starts-with(' + attr_types[
+        'class'] + ', "entry-categories") or ' + attr_types['class'] + '="postinfo" or ' + attr_types[
+        'id'] + '="filedunder"]//a',
+    '//footer[starts-with(' + attr_types['class'] + ', "entry-meta") or starts-with(' + attr_types[
+        'class'] + ', "entry-footer")]//a',
+    '//*[(self::li or self::span)][' + attr_types['class'] + '="post-category" or ' + attr_types[
+        'class'] + '="postcategory" or ' + attr_types['class'] + '="entry-category"]//a',
+    '//header[' + attr_types['class'] + '="entry-header"]//a',
+    '//div[' + attr_types['class'] + '="row" or ' + attr_types['class'] + '="tags"]//a',
 ]
 # "//div[contains(@class, 'byline')]",
 # "//p[contains(@class, 'byline')]",
@@ -31,12 +45,12 @@ categories_xpaths = [
 
 
 tags_xpaths = [
-    '//div[@class="tags"]//a',
-    "//p[starts-with(@class, 'entry-tags')]//a",
-    '''//div[@class="row" or @class="jp-relatedposts" or
-    @class="entry-utility" or starts-with(@class, 'tag') or
-    starts-with(@class, 'postmeta') or starts-with(@class, 'meta')]//a''',
-    '//*[@class="entry-meta" or contains(@class, "topics")]//a',
+    '//div[' + attr_types['class'] + '="tags"]//a',
+    '//p[starts-with(' + attr_types['class'] + ', "entry-tags")]//a',
+    '//div[' + attr_types['class'] + '="row" or ' + attr_types['class'] + '="jp-relatedposts" or ' + attr_types[
+        'class'] + '="entry-utility" or starts-with(' + attr_types['class'] + ', "tag") or starts-with(' + attr_types[
+        'class'] + ', "postmeta") or starts-with(' + attr_types['class'] + ', "meta")]//a',
+    '//*[' + attr_types['class'] + '="entry-meta" or contains(' + attr_types['class'] + ', "topics")]//a',
 ]
 # span class tag-links
 # "related-topics"
@@ -44,9 +58,12 @@ tags_xpaths = [
 
 
 title_xpaths = [
-    '//*[(self::h1 or self::h2)][contains(@class, "post-title") or contains(@class, "entry-title") or contains(@class, "headline") or contains(@id, "headline") or contains(@itemprop, "headline") or contains(@class, "post__title")]',
-    '//*[@class="entry-title" or @class="post-title"]',
-    '//h1[contains(@class, "title") or contains(@id, "title")]',
+    '//*[(self::h1 or self::h2)][contains(' + attr_types['class'] + ', "post-title") or contains(' + attr_types[
+        'class'] + ', "entry-title") or contains(' + attr_types['class'] + ', "headline") or contains(' + attr_types[
+        'id'] + ', "headline") or contains(' + attr_types['itemprop'] + ', "headline") or contains(' + attr_types[
+        'class'] + ', "post__title")]',
+    '//*[' + attr_types['class'] + '="entry-title" or ' + attr_types['class'] + '="post-title"]',
+    '//h1[contains(' + attr_types['class'] + ', "title") or contains(' + attr_types['id'] + ', "title")]',
 ]
 # json-ld headline
 # '//header/h1',
