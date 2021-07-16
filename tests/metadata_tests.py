efbd7ee51b1e468e51072fed17f65cc98ceb3b8c
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2021-07-16 19:01:44 +1000

    Added xpath to check any id that contains author

--- tests/metadata_tests.py
+++ tests/metadata_tests.py
@@ -141,6 +141,8 @@ def test_authors():
     assert metadata['author'] == 'Jenny Smith'
     metadata = extract_metadata('<html><body><span class="author">Jenny Smith</span></body></html>')
     assert metadata['author'] == 'Jenny Smith'
+    metadata = extract_metadata('<html><body><span id="author-name">Jenny Smith</span></body></html>')
+    assert metadata['author'] == 'Jenny Smith'
     metadata = extract_metadata('<html><body><span class="author">Jenny Smith and John Smith</span></body></html>')
     assert metadata['author'] == 'Jenny Smith; John Smith'
     metadata = extract_metadata('<html><body><a class="author">Jenny Smith</a></body></html>')
