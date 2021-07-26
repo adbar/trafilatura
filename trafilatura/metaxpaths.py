d758daa58fdc5b2d024f282869110809eaea2347
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2021-07-26 21:17:37 +1000

    Added tag strong on xpath

--- trafilatura/metaxpaths.py
+++ trafilatura/metaxpaths.py
@@ -8,8 +8,8 @@ File containing XPath expressions to extract metadata.
 
 # the order or depth of XPaths could be changed after exhaustive testing
 author_xpaths = [
-    '//*[(self::a or self::address or self::link or self::p or self::span)][@rel="author" or @id="author" or @class="author" or @itemprop="author name" or rel="me"]|//author', # specific
-    '//*[(self::a or self::div or self::span or self::p)][contains(@class, "author-name") or contains(@class, "authorName") or contains(@class, "author name")]', # almost specific
+    '//*[(self::a or self::address or self::link or self::p or self::span or self::strong)][@rel="author" or @id="author" or @class="author" or @itemprop="author name" or rel="me"]|//author', # specific
+    '//*[(self::a or self::div or self::span or self::p or self::strong)][contains(@class, "author-name") or contains(@class, "authorName") or contains(@class, "author name")]', # almost specific
     '//*[(self::a or self::div or self::span or self::p)][contains(@class, "author") or contains(@id, "author") or contains(@itemprop, "author") or @class="byline"]', # almost generic
     '//*[(self::a or self::div or self::span or self::p)][contains(@class, "authors") or contains(@class, "byline") or contains(@class, "submitted-by") or contains(@class, "posted-by")]', # generic
     '//*[contains(@class, "author") or contains(@class, "Author") or contains(@id, "Author") or contains(@class, "screenname") or contains(@data-component, "Byline") or contains(@itemprop, "author") or contains(@class, "writer")]', # any element
