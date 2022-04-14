56416ffef3d48df5c86d78245bcae4a8bc8fd3fa
administrator <administrator@50031.local>
2022-04-14 17:58:46 +0100

    Completed Coinbase article annotation.

--- tests/evaldata.py
+++ tests/evaldata.py
@@ -2,9 +2,9 @@
 EVAL_PAGES = {
 
 'https://www.thebalance.com/coinbase-vs-coinbase-pro-5116733': {
-    'file': 'coinbase-vs-coinbase-pro-5116733’
-    'with': 
-    'without':
+    'file': 'coinbase-vs-coinbase-pro-5116733’,
+    'with': ['Coinbase vs. Coinbase Pro', 'How do they compare?', 'Buy/sell with debit cards'],
+    'without': ['The Balance is part of the Dotdash Meredith publishing family.', 'Kraken vs. Coinbase: How Do They compare?', 'the balance', 'FOLLOW US']
 },
 
 
