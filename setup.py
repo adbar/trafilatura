ea5be0b74ebc8dbb59d4192149bfefdb1db779f3
Adrien Barbaresi <barbaresi@bbaw.de>
2022-12-13 17:33:09 +0100

    setup: use faust-cchardet from 3.10 onwards

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ def get_long_description():
 # some problems with installation solved this way
 extras = {
     "all": [
-        "cchardet >= 2.1.7; python_version < '3.11'",  # build issue
-        "faust-cchardet >= 2.1.12; python_version >= '3.11'",  # fix for build
+        "cchardet >= 2.1.7; python_version < '3.10'",  # build issue
+        "faust-cchardet >= 2.1.12; python_version >= '3.10'",  # fix for build
         "htmldate[speed] >= 1.4.0",
         "py3langid >= 0.2.2",
         "pycurl >= 7.45.1",
