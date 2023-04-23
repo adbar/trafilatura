aece9fa8666b384689bdbd8cdabd69d9e6e750de
Korben <korben@korben.info>
2023-04-23 17:58:04 +0200

    Update core.py
    When I want to use a lambda function in python like this one :
    
    'extract_content = lambda url: trafilatura.extract(trafilatura.fetch_url(url), output_format='html', include_links=True)'
    
    
    I encounter the following error: 
    
    'AttributeError: 'lxml.etree._Element' object has no attribute 'text_content''
    
    Indeed, in the trafilatura library, the text_content() method is used to extract the text from the _Element object of lxml.etree. We can get the same result by using the etree.tostring() method with the argument method='text'.
    
    So in the compare_extraction function of trafilatura/core.py, I replaced this line :
    
    'algo_text = trim(temppost_algo.text_content())'
    
    with this line: 
    
    'algo_text = trim(etree.tostring(temppost_algo, method='text', encoding='utf-8').decode('utf-8'))'
    
    
    Don't forget to import etree in core.py
    
    'from lxml import etree'
    
    Tested and working

--- trafilatura/core.py
+++ trafilatura/core.py
@@ -13,6 +13,7 @@ import re  # import regex as re
 import warnings
 
 from copy import deepcopy
+from lxml import etree
 
 # SIGALRM isn't present on Windows, detect it
 try:
@@ -642,7 +643,7 @@ def compare_extraction(tree, backup_tree, url, body, text, len_text, options):
         backup_tree = prune_unwanted_nodes(backup_tree, OVERALL_DISCARD_XPATH)
     # try with readability
     temppost_algo = try_readability(backup_tree)
-    algo_text = trim(temppost_algo.text_content())
+    algo_text = trim(etree.tostring(temppost_algo, method='text', encoding='utf-8').decode('utf-8'))
     len_algo = len(algo_text)
     # compare
     LOGGER.debug('extracted length: %s (algorithm) %s (extraction)', len_algo, len_text)
