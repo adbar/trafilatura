bacd9b588527d3518dc62704b8d11105b235b2f9
Adrien Barbaresi <adbar@users.noreply.github.com>
2023-05-23 14:45:52 +0200

    sitemaps: use class and simplify code structure (#350)
    * sitemaps: simplify code structure
    
    * handle_link() as method
    
    * simplify code
    
    * simplify further
    
    * add processing function to class
    
    * add safety checks
    
    * polish code

--- tests/sitemaps_tests.py
+++ tests/sitemaps_tests.py
@@ -18,27 +18,31 @@ RESOURCES_DIR = os.path.join(TEST_DIR, 'resources')
 
 def test_search():
     '''Test search for sitemaps'''
-    assert sitemaps.sitemap_search('12345') == []
-    assert sitemaps.sitemap_search('12345.xml.gz') == []
-    assert sitemaps.sitemap_search('https://1.net/sitemap.xml.gz') == []
-    assert sitemaps.sitemap_search('https://bogusdomain.net/') == []
+    assert not sitemaps.sitemap_search('12345')
+    assert not sitemaps.sitemap_search('12345.xml.gz')
+    assert not sitemaps.sitemap_search('https://1.net/sitemap.xml.gz')
+    assert not sitemaps.sitemap_search('https://bogusdomain.net/')
 
 
 def test_extraction():
     '''Test simple link extraction'''
     # link handling
     url, domain, baseurl = 'https://www.sitemaps.org/sitemap.xml', 'sitemaps.org', 'https://www.sitemaps.org'
-    sitemap = sitemaps.SitemapObject(baseurl, "", domain, url)
-    assert sitemaps.handle_link(url, sitemap) == (url, '0')
+    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap.handle_link(url)
+    assert not sitemap.sitemap_urls and not sitemap.urls
 
-    sitemap = sitemaps.SitemapObject('https://example.org', "", 'example.org', 'https://example.org/sitemap.xml')
-    assert sitemaps.handle_link('https://mydomain', sitemap) == ('https://mydomain', '0')
+    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', 'https://example.org/sitemap.xml')
+    sitemap.handle_link('https://mydomain')
+    assert not sitemap.sitemap_urls and not sitemap.urls
 
-    sitemap = sitemaps.SitemapObject('https://example.org', "", 'example.org', 'https://example.org/sitemap.xml')
-    assert sitemaps.handle_link('https://mydomain.wordpress.com/1', sitemap) == ('https://mydomain.wordpress.com/1', 'link')
+    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', 'https://example.org/sitemap.xml')
+    sitemap.handle_link('https://mydomain.wordpress.com/1')
+    assert not sitemap.sitemap_urls and sitemap.urls == ['https://mydomain.wordpress.com/1']
 
-    sitemap = sitemaps.SitemapObject('https://programtalk.com', "", 'programtalk.com', 'https://programtalk.com/sitemap.xml')
-    assert sitemaps.handle_link('http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent', sitemap) == ('http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent', 'link')
+    sitemap = sitemaps.SitemapObject('https://programtalk.com', 'programtalk.com', 'https://programtalk.com/sitemap.xml')
+    sitemap.handle_link('http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent')
+    assert not sitemap.sitemap_urls and sitemap.urls == ['http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent']
 
     # similar domain names
     assert not is_similar_domain('kleins-weindepot.de', 'eurosoft.net')
@@ -49,21 +53,23 @@ def test_extraction():
     url = 'https://de.sitemaps.org/1'
     sitemap_url = 'https://de.sitemaps.org/sitemap.xml'
     domain, baseurl = get_hostinfo(sitemap_url)
-    sitemap = sitemaps.SitemapObject(baseurl, "", domain, sitemap_url)
-    assert sitemaps.handle_link(url, sitemap) == (url, 'link')
+    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap.handle_link(url)
+    assert not sitemap.sitemap_urls and sitemap.urls == [url]
 
     # diverging domains
     url = 'https://www.software.info/1'
     sitemap_url = 'https://example.org/sitemap.xml'
     domain, baseurl = get_hostinfo(sitemap_url)
-    sitemap = sitemaps.SitemapObject(baseurl, "", domain, sitemap_url)
-    assert sitemaps.handle_link(url, sitemap) == (url, '0')
+    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap.handle_link(url)
+    assert not sitemap.sitemap_urls and not sitemap.urls
 
     # don't take this one?
     #url = 'https://subdomain.sitemaps.org/1'
     #sitemap_url = 'https://www.sitemaps.org/sitemap.xml'
     #domain, baseurl = get_hostinfo(sitemap_url)
-    #assert sitemaps.handle_link(url, sitemap_url, domain, baseurl, None) == (url, '0')
+    #sitemap.handle_link(url)  #  (url, '0')
 
     # safety belts
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap.xml.gz', b'\x1f\x8bABC') is False
@@ -71,40 +77,49 @@ def test_extraction():
     assert sitemaps.is_plausible_sitemap('http://test.org/sitemap.xml', '<!DOCTYPE html><html><body/></html>') is False
     assert sitemaps.is_plausible_sitemap('http://test.org/sitemap', '<!DOCTYPE html><html><body/></html>') is False
     # invalid
-    sitemap = sitemaps.SitemapObject(baseurl, domain, '<html>\n</html>', url)
-    assert sitemaps.extract_sitemap_links(sitemap) == ([], [])
+    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap.content = '<html>\n</html>'
+    sitemap.extract_sitemap_links()
+    assert not sitemap.sitemap_urls and not sitemap.urls
 
     # parsing a file
     url, domain, baseurl = 'http://www.sitemaps.org/sitemap.xml', 'sitemaps.org', 'http://www.sitemaps.org'
     filepath = os.path.join(RESOURCES_DIR, 'sitemap.xml')
-    with open(filepath) as f:
+    with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
     assert sitemaps.is_plausible_sitemap('http://sitemaps.org/sitemap.xml', teststring) is True
-    sitemap = sitemaps.SitemapObject(baseurl, teststring, domain, sitemap_url)
-    sitemapurls, linklist = sitemaps.extract_sitemap_links(sitemap)
-    assert len(sitemapurls) == 0 and len(linklist) == 84
+    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap.content = teststring
+    sitemap.extract_sitemap_links()
+    assert not sitemap.sitemap_urls and len(sitemap.urls) == 84
     # hreflang
-    assert sitemaps.extract_sitemap_langlinks(sitemap) == ([], [])
+    sitemap.urls = []
+    sitemap.extract_sitemap_langlinks()
+    assert not sitemap.sitemap_urls and not sitemap.urls
 
     # nested sitemaps
     url, domain, baseurl = 'http://www.example.com/sitemap.xml', 'example.com', 'http://www.example.com'
     filepath = os.path.join(RESOURCES_DIR, 'sitemap2.xml')
-    with open(filepath) as f:
+    with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
-    sitemap = sitemaps.SitemapObject(baseurl, teststring, domain, url)
-    sitemapurls, linklist = sitemaps.extract_sitemap_links(sitemap)
-    assert sitemapurls == ['http://www.example.com/sitemap1.xml.gz', 'http://www.example.com/sitemap2.xml.gz'] and linklist == []
+    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap.content = teststring
+    sitemap.extract_sitemap_links()
+    assert sitemap.sitemap_urls == ['http://www.example.com/sitemap1.xml.gz', 'http://www.example.com/sitemap2.xml.gz'] and not sitemap.urls
 
     # hreflang
-    sitemap = sitemaps.SitemapObject('https://test.org/', '<?xml version="1.0" encoding="UTF-8"?><urlset><url><loc>http://www.test.org/english/page.html</loc></url></urlset>', 'test.org', 'https://test.org/sitemap', target_lang='en')
-    assert sitemaps.process_sitemap(sitemap) == ([], ['http://www.test.org/english/page.html'])
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap', 'en')
+    sitemap.content = '<?xml version="1.0" encoding="UTF-8"?><urlset><url><loc>http://www.test.org/english/page.html</loc></url></urlset>'
+    sitemap.process()
+    assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['http://www.test.org/english/page.html'])
     filepath = os.path.join(RESOURCES_DIR, 'sitemap-hreflang.xml')
-    with open(filepath) as f:
+    with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
-    sitemap = sitemaps.SitemapObject(baseurl, teststring, domain, url, target_lang='de')
-    sitemapsurls, linklist = sitemaps.extract_sitemap_langlinks(sitemap)
-    assert sitemapsurls == ['http://www.example.com/sitemap-de.xml.gz']
-    assert len(linklist) > 0
+    sitemap = sitemaps.SitemapObject(baseurl, domain, url, 'de')
+    sitemap.content = teststring
+    sitemap.extract_sitemap_langlinks()
+    assert sitemap.sitemap_urls == ['http://www.example.com/sitemap-de.xml.gz']
+    assert len(sitemap.urls) > 0
 
     # GZ-compressed sitemaps
     url, domain, baseurl = 'https://www.sitemaps.org/sitemap.xml', 'sitemaps.org', 'https://www.sitemaps.org'
@@ -113,9 +128,10 @@ def test_extraction():
         teststring = f.read()
     teststring = decode_response(teststring)
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap.xml.gz', teststring) is True
-    sitemap = sitemaps.SitemapObject(baseurl, teststring, domain, url)
-    sitemapurls, linklist = sitemaps.extract_sitemap_links(sitemap)
-    assert len(sitemapurls) == 0 and len(linklist) == 84
+    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap.content = teststring
+    sitemap.extract_sitemap_links()
+    assert len(sitemap.sitemap_urls) == 0 and len(sitemap.urls) == 84
 
     # check contents
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap.xml.gz?value=1', teststring) is True
@@ -123,25 +139,36 @@ def test_extraction():
     # TXT links
     content = 'Tralala\nhttps://test.org/1\nhttps://test.org/2'
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap', content) is True
-    sitemap = sitemaps.SitemapObject('https://test.org/', 'Tralala\nhttps://test.org/1\nhttps://test.org/2', 'test.org', 'https://test.org/sitemap')
-    assert sitemaps.process_sitemap(sitemap) == ([], ['https://test.org/1', 'https://test.org/2'])
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap')
+    sitemap.content = 'Tralala\nhttps://test.org/1\nhttps://test.org/2'
+    sitemap.process()
+    assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['https://test.org/1', 'https://test.org/2'])
 
     # TXT links + language
-    sitemap = sitemaps.SitemapObject('https://test.org/', 'Tralala\nhttps://test.org/en/1\nhttps://test.org/en/2\nhttps://test.org/es/3', 'test.org', 'https://test.org/sitemap', target_lang='en')
-    assert sitemaps.process_sitemap(sitemap) == ([], ['https://test.org/en/1', 'https://test.org/en/2'])
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap', 'en')
+    sitemap.content = 'Tralala\nhttps://test.org/en/1\nhttps://test.org/en/2\nhttps://test.org/es/3'
+    sitemap.process()
+    assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['https://test.org/en/1', 'https://test.org/en/2'])
 
 
 def test_robotstxt():
     '''Check if sitemaps can be found over robots.txt'''
-    assert sitemaps.find_robots_sitemaps('https://http.org') == []
+    assert not sitemaps.find_robots_sitemaps('https://http.org')
     baseurl = 'https://httpbun.org'
-    assert sitemaps.find_robots_sitemaps(baseurl) == []
-    assert sitemaps.extract_robots_sitemaps('# test', baseurl) == []
-    assert sitemaps.extract_robots_sitemaps('# test'*10000, baseurl) == []
+    assert not sitemaps.find_robots_sitemaps(baseurl)
+    assert not sitemaps.extract_robots_sitemaps('# test', baseurl)
+    assert not sitemaps.extract_robots_sitemaps('# test'*10000, baseurl)
     assert sitemaps.extract_robots_sitemaps('sitemap: https://example.org/sitemap.xml', baseurl) == ['https://example.org/sitemap.xml']
+
+
+def test_whole():
+    "Test whole process."
+    results = sitemaps.sitemap_search("https://www.sitemaps.org", target_lang="de")
+    assert len(results) == 8
 
 
 if __name__ == '__main__':
     test_search()
     test_extraction()
     test_robotstxt()
+    test_whole()
--- trafilatura/sitemaps.py
+++ trafilatura/sitemaps.py
@@ -12,7 +12,7 @@ import re
 # ROBOT_PARSER = urllib.robotparser.RobotFileParser()
 
 from itertools import islice
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 from courlan import clean_url, extract_domain, filter_urls, fix_relative_urls, get_hostinfo, lang_filter
 
@@ -39,14 +39,89 @@ GUESSES = ['sitemap.xml.gz', 'sitemap', 'sitemap_index.xml', 'sitemap_news.xml']
 
 class SitemapObject:
     "Store all necessary information on sitemap download and processing."
-    __slots__ = ["base_url", "content", "domain", "sitemap_url", "target_lang"]
+    __slots__ = ["base_url", "content", "domain", "sitemap_url", "sitemap_urls", "target_lang", "urls"]
 
-    def __init__(self, base_url: str, content: str, domain: str, sitemap_url: str, target_lang: Optional[str] = None) -> None:
+    def __init__(self, base_url: str, domain: str, sitemap_url: str, target_lang: Optional[str] = None) -> None:
         self.base_url: str = base_url
-        self.content: str = content
+        self.content: str = ""
         self.domain: str = domain
         self.sitemap_url: str = sitemap_url
+        self.sitemap_urls: List[str] = []
         self.target_lang: Optional[str] = target_lang
+        self.urls: List[str] = []
+
+    def fetch(self) -> None:
+        "Fetch a sitemap over the network."
+        LOGGER.debug('fetching sitemap: %s', self.sitemap_url)
+        self.content = fetch_url(self.sitemap_url)
+
+    def handle_link(self, link: str) -> None:
+        """Examine a link and determine if it's valid and if it leads to
+           a sitemap or a web page."""
+        if link == self.sitemap_url:  # safety check
+            return
+        # fix, check, clean and normalize
+        link = fix_relative_urls(self.base_url, link)
+        link = clean_url(link, self.target_lang)
+
+        if link is None or not lang_filter(link, self.target_lang):
+            return
+
+        newdomain = extract_domain(link, fast=True)
+        if newdomain is None:
+            LOGGER.error("couldn't extract domain: %s", link)
+            return
+
+        # don't take links from another domain and make an exception for main platforms
+        # also bypass: subdomains vs. domains
+        if not is_similar_domain(self.domain, newdomain) and not WHITELISTED_PLATFORMS.search(newdomain):
+            LOGGER.warning('link discarded, diverging domain names: %s %s', self.domain, newdomain)
+            return
+
+        if DETECT_SITEMAP_LINK.search(link):
+            self.sitemap_urls.append(link)
+        else:
+            self.urls.append(link)
+
+    def extract_sitemap_langlinks(self) -> None:
+        "Extract links corresponding to a given target language."
+        if 'hreflang=' not in self.content:
+            return
+        # compile regex here for modularity and efficiency
+        lang_regex = re.compile(rf"hreflang=[\"']({self.target_lang}.*?|x-default)[\"']", re.DOTALL)
+        # extract
+        for attrs in (m[0] for m in islice(XHTML_REGEX.finditer(self.content), MAX_LINKS)):
+            if lang_regex.search(attrs):
+                lang_match = HREFLANG_REGEX.search(attrs)
+                if lang_match:
+                    self.handle_link(lang_match[1])
+        LOGGER.debug('%s sitemaps and %s links with hreflang found for %s', len(self.sitemap_urls), len(self.urls), self.sitemap_url)
+
+    def extract_sitemap_links(self) -> None:
+        "Extract sitemap links and web page links from a sitemap file."
+        # extract
+        for match in (m[1] for m in islice(LINK_REGEX.finditer(self.content), MAX_LINKS)):
+            # process middle part of the match tuple
+            self.handle_link(match)
+        LOGGER.debug('%s sitemaps and %s links found for %s', len(self.sitemap_urls), len(self.urls), self.sitemap_url)
+
+    def process(self) -> None:
+        "Download a sitemap and extract the links it contains."
+        plausible = is_plausible_sitemap(self.sitemap_url, self.content)
+        # safeguard
+        if not plausible:
+            return
+        # try to extract links from TXT file
+        if not SITEMAP_FORMAT.match(self.content):
+            for match in (m[0] for m in islice(DETECT_LINKS.finditer(self.content), MAX_LINKS)):
+                self.handle_link(match)
+            return
+        # process XML sitemap
+        if self.target_lang is not None:
+            self.extract_sitemap_langlinks()
+            if self.sitemap_urls or self.urls:
+                return
+        self.extract_sitemap_links()
 
 
 def sitemap_search(url: str, target_lang: Optional[str] = None) -> List[str]:
@@ -66,46 +141,54 @@ def sitemap_search(url: str, target_lang: Optional[str] = None) -> List[str]:
     if domainname is None:
         LOGGER.warning('invalid URL: %s', url)
         return []
-    # check base URL
+
     if not is_live_page(baseurl):
         LOGGER.warning('base URL unreachable, dropping sitemap: %s', url)
         return []
-    # determine sitemap URL
+
     urlfilter = None
-    if url.endswith('.xml') or url.endswith('.gz') or url.endswith('sitemap'):
+    if url.endswith(('.gz', 'sitemap', '.xml')):
         sitemapurl = url
     else:
         sitemapurl = baseurl + '/sitemap.xml'
         # filter triggered, prepare it
         if len(url) > len(baseurl) + 2:
             urlfilter = url
-    sitemapurls, linklist = download_and_process_sitemap(sitemapurl, domainname, baseurl, target_lang)
-    if sitemapurls == [] and len(linklist) > 0:
-        linklist = filter_urls(linklist, urlfilter)
+
+    sitemap = SitemapObject(baseurl, domainname, sitemapurl, target_lang)
+    sitemap.fetch()
+    sitemap.process()
+
+    if not sitemap.sitemap_urls and sitemap.urls:
+        linklist = filter_urls(sitemap.urls, urlfilter)
         LOGGER.debug('%s sitemap links found for %s', len(linklist), domainname)
         return linklist
+
     # try sitemaps in robots.txt file if nothing has been found
-    if sitemapurls == [] and linklist == []:
-        sitemapurls = find_robots_sitemaps(baseurl)
+    if not sitemap.sitemap_urls and not sitemap.urls:
+        sitemap.sitemap_urls = find_robots_sitemaps(baseurl)
         # try additional URLs just in case
-        if not sitemapurls:
-            sitemapurls = [''.join([baseurl, '/', g]) for g in GUESSES]
+        if not sitemap.sitemap_urls:
+            sitemap.sitemap_urls = [''.join([baseurl, '/', g]) for g in GUESSES]
+
     # iterate through nested sitemaps and results
+    seen = {sitemapurl}
     i = 1
-    sitemaps_seen = {sitemapurl}
-    while sitemapurls:
-        sitemapurl = sitemapurls.pop()
-        sitemapurls, linklist = download_and_process_sitemap(sitemapurl, domainname, baseurl, target_lang, sitemapurls, linklist)
+    while sitemap.sitemap_urls:
+        sitemap.sitemap_url = sitemap.sitemap_urls.pop()
+        sitemap.fetch()
+        sitemap.process()
         # sanity check: keep track of visited sitemaps and exclude them
-        sitemaps_seen.add(sitemapurl)
-        sitemapurls = [s for s in sitemapurls if s not in sitemaps_seen]
+        seen.add(sitemap.sitemap_url)
+        sitemap.sitemap_urls = [s for s in sitemap.sitemap_urls if s not in seen]
         # counter and safeguard
         i += 1
         if i > MAX_SITEMAPS_SEEN:
             break
-    linklist = filter_urls(linklist, urlfilter)
-    LOGGER.debug('%s sitemap links found for %s', len(linklist), domainname)
-    return linklist
+
+    sitemap.urls = filter_urls(sitemap.urls, urlfilter)
+    LOGGER.debug('%s sitemap links found for %s', len(sitemap.urls), domainname)
+    return sitemap.urls
 
 
 def is_plausible_sitemap(url: str, contents: Optional[str]) -> bool:
@@ -113,112 +196,18 @@ def is_plausible_sitemap(url: str, contents: Optional[str]) -> bool:
        i.e. TXT or XML.'''
     if contents is None:
         return False
+
     # strip query and fragments
     url = SCRUB_REGEX.sub('', url)
+
     # check content
     if POTENTIAL_SITEMAP.search(url) and \
         (not isinstance(contents, str) or not SITEMAP_FORMAT.match(contents)) \
          or '<html' in contents[:150].lower():
         LOGGER.warning('not a valid XML sitemap: %s', url)
         return False
+
     return True
-
-
-def download_and_process_sitemap(url: str, domain: str, baseurl: str, target_lang: Optional[str], sitemapurls: Optional[List[str]] = None, linklist: Optional[List[str]] = None) -> Tuple[List[str], List[str]]:
-    'Helper function chaining download and processing of sitemaps.'
-    # variables init
-    sitemapurls, linklist = sitemapurls or [], linklist or []
-    # fetch and pre-process
-    LOGGER.debug('fetching sitemap: %s', url)
-    pagecontent = fetch_url(url)
-    sitemap = SitemapObject(baseurl, pagecontent, domain, url, target_lang)
-    add_sitemaps, add_links = process_sitemap(sitemap)
-    return sitemapurls + add_sitemaps, linklist + add_links
-
-
-def process_sitemap(sitemap: SitemapObject) -> Tuple[List[str], List[str]]:
-    'Download a sitemap and extract the links it contains.'
-    plausible = is_plausible_sitemap(sitemap.sitemap_url, sitemap.content)
-    # safeguard
-    if not plausible:
-        return [], []
-    # try to extract links from TXT file
-    if not SITEMAP_FORMAT.match(sitemap.content):
-        sitemapurls, linklist = [], []  # type: List[str], List[str]
-        for match in (m[0] for m in islice(DETECT_LINKS.finditer(sitemap.content), MAX_LINKS)):
-            link, state = handle_link(match, sitemap)
-            sitemapurls, linklist = store_sitemap_link(sitemapurls, linklist, link, state)
-        return sitemapurls, linklist
-    # process XML sitemap
-    if sitemap.target_lang is not None:
-        sitemapurls, linklist = extract_sitemap_langlinks(sitemap)
-        if len(sitemapurls) != 0 or len(linklist) != 0:
-            return sitemapurls, linklist
-    return extract_sitemap_links(sitemap)
-
-
-def handle_link(link: str, sitemap: SitemapObject) -> Tuple[str, str]:
-    '''Examine a link and determine if it's valid and if it leads to
-       a sitemap or a web page.'''
-    state = '0'
-    # safety net: recursivity
-    if link == sitemap.sitemap_url:
-        return link, state
-    # fix and check
-    link = fix_relative_urls(sitemap.base_url, link)
-    # clean and normalize
-    link = clean_url(link, sitemap.target_lang)
-    if link is not None and lang_filter(link, sitemap.target_lang) is True:
-        newdomain = extract_domain(link, fast=True)
-        if newdomain is None:
-            LOGGER.error("couldn't extract domain: %s", link)
-        # don't take links from another domain and make an exception for main platforms
-        # also bypass: subdomains vs. domains
-        elif not is_similar_domain(sitemap.domain, newdomain) and not WHITELISTED_PLATFORMS.search(newdomain):
-            LOGGER.warning('link discarded, diverging domain names: %s %s', sitemap.domain, newdomain)
-        else:
-            state = 'sitemap' if DETECT_SITEMAP_LINK.search(link) else 'link'
-    return link, state
-
-
-def store_sitemap_link(sitemapurls: List[str], linklist: List[str], link: str, state: str) -> Tuple[List[str], List[str]]:
-    '''Process link according to filtered result: discard it or store it
-       in the appropriate list.'''
-    if state == 'sitemap' and link is not None:
-        sitemapurls.append(link)
-    elif state == 'link' and link is not None:
-        linklist.append(link)
-    return sitemapurls, linklist
-
-
-def extract_sitemap_langlinks(sitemap: SitemapObject) -> Tuple[List[str], List[str]]:
-    'Extract links corresponding to a given target language.'
-    if 'hreflang=' not in sitemap.content:
-        return [], []
-    sitemapurls, linklist = [], []  # type: List[str], List[str]
-    # compile regex here for modularity and efficiency
-    lang_regex = re.compile(rf"hreflang=[\"']({sitemap.target_lang}.*?|x-default)[\"']", re.DOTALL)
-    # extract
-    for attrs in (m[0] for m in islice(XHTML_REGEX.finditer(sitemap.content), MAX_LINKS)):
-        if lang_regex.search(attrs):
-            lang_match = HREFLANG_REGEX.search(attrs)
-            if lang_match:
-                link, state = handle_link(lang_match[1], sitemap)
-                sitemapurls, linklist = store_sitemap_link(sitemapurls, linklist, link, state)
-    LOGGER.debug('%s sitemaps and %s links with hreflang found for %s', len(sitemapurls), len(linklist), sitemap.sitemap_url)
-    return sitemapurls, linklist
-
-
-def extract_sitemap_links(sitemap: SitemapObject) -> Tuple[List[str], List[str]]:
-    'Extract sitemap links and web page links from a sitemap file.'
-    sitemapurls, linklist = [], []  # type: List[str], List[str]
-    # extract
-    for match in (m[1] for m in islice(LINK_REGEX.finditer(sitemap.content), MAX_LINKS)):
-        # process middle part of the match tuple
-        link, state = handle_link(match, sitemap)
-        sitemapurls, linklist = store_sitemap_link(sitemapurls, linklist, link, state)
-    LOGGER.debug('%s sitemaps and %s links found for %s', len(sitemapurls), len(linklist), sitemap.sitemap_url)
-    return sitemapurls, linklist
 
 
 def find_robots_sitemaps(baseurl: str) -> List[str]:
