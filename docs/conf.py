33966a27f5f0bd9d921d058a8501cded00cf3d1c
Tony Yang <tonyyanga@gmail.com>
2023-10-26 17:04:02 -0700

    add sphinx-sitemap

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx_sitemap'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -135,3 +136,5 @@ html_context = {
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
 }
+
+html_baseurl = 'https://trafilatura.readthedocs.io/'
