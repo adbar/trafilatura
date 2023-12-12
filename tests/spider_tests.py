b0a8b5c51fecd1f9cebaacf65f87c03af4b98b64
Adrien Barbaresi <barbaresi@bbaw.de>
2023-12-12 19:14:43 +0100

    uncomment tests

--- tests/spider_tests.py
+++ tests/spider_tests.py
@@ -153,9 +153,8 @@ def test_focused_crawler():
     "Test the whole focused crawler mechanism."
     spider.URL_STORE = UrlStore()
     todo, known_links = spider.focused_crawler("https://httpbun.com/links/1/1", max_seen_urls=1)
-    ## TODO: check this on Github actions:
-    # assert sorted(known_links) == ['https://httpbun.com/links/1/0', 'https://httpbun.com/links/1/1']
-    # assert sorted(todo) == ['https://httpbun.com/links/1/0']
+    assert sorted(known_links) == ['https://httpbun.com/links/1/0', 'https://httpbun.com/links/1/1']
+    assert sorted(todo) == ['https://httpbun.com/links/1/0']
 
 
 if __name__ == '__main__':
