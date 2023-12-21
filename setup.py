aaf49c474eda74a8f64968f19e12fafa474fa279
Adrien Barbaresi <barbaresi@bbaw.de>
2023-12-21 15:36:49 +0100

    fix setup on MacOS and add tests

--- setup.py
+++ setup.py
@@ -115,8 +115,9 @@ setup(
         "htmldate >= 1.6.0",
         "importlib_metadata; python_version < '3.8'",
         "justext >= 3.0.0",
-        "lxml >= 4.9.3 ; platform_system != 'Darwin'",
-        "lxml == 4.9.2 ; platform_system == 'Darwin'",
+        # see tests on Github Actions
+        "lxml == 4.9.2 ; platform_system == 'Darwin' and python_version <= '3.8'",
+        "lxml >= 4.9.4 ; platform_system != 'Darwin' or python_version > '3.8'",
         "urllib3 >= 1.26, < 2; python_version < '3.7'",
         "urllib3 >= 1.26, < 3; python_version >= '3.7'",
     ],
