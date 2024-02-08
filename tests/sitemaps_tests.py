c8f978dfa5791716a7d0fe34d496c95ad79ea9e7
Adrien Barbaresi <adbar@users.noreply.github.com>
2024-02-08 13:18:12 +0100

    sitemaps: simplify and improve search (#503)
    * simplify and improve sitemap init
    
    * better coverage
    
    * fix len test
    
    * shorter code

--- tests/sitemaps_tests.py
+++ tests/sitemaps_tests.py
@@ -8,8 +8,9 @@ import sys
 
 from courlan import get_hostinfo
 
+import trafilatura
 from trafilatura import sitemaps
-from trafilatura.utils import decode_response, is_similar_domain
+from trafilatura.utils import decode_file, is_similar_domain
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -29,21 +30,28 @@ def test_extraction():
     '''Test simple link extraction'''
     # link handling
     url, domain, baseurl = 'https://www.sitemaps.org/sitemap.xml', 'sitemaps.org', 'https://www.sitemaps.org'
-    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [])
     sitemap.handle_link(url)
-    assert not sitemap.sitemap_urls and not sitemap.urls
+    assert len(sitemap.sitemap_urls) == 1 and not sitemap.urls
 
-    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', 'https://example.org/sitemap.xml')
+    # same URL
+    url, domain, baseurl = 'https://www.sitemaps.org/sitemap.xml', 'sitemaps.org', 'https://www.sitemaps.org'
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [url])
+    sitemap.current_url = url
+    sitemap.handle_link(url)
+    assert len(sitemap.sitemap_urls) == 1 and not sitemap.urls
+
+    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', ['https://example.org/sitemap.xml'])
     sitemap.handle_link('https://mydomain')
-    assert not sitemap.sitemap_urls and not sitemap.urls
+    assert len(sitemap.sitemap_urls) == 1 and not sitemap.urls
 
-    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', 'https://example.org/sitemap.xml')
+    sitemap = sitemaps.SitemapObject('https://example.org', 'example.org', ['https://example.org/sitemap.xml'])
     sitemap.handle_link('https://mydomain.wordpress.com/1')
-    assert not sitemap.sitemap_urls and sitemap.urls == ['https://mydomain.wordpress.com/1']
+    assert len(sitemap.sitemap_urls) == 1 and sitemap.urls == ['https://mydomain.wordpress.com/1']
 
-    sitemap = sitemaps.SitemapObject('https://programtalk.com', 'programtalk.com', 'https://programtalk.com/sitemap.xml')
+    sitemap = sitemaps.SitemapObject('https://programtalk.com', 'programtalk.com', ['https://programtalk.com/sitemap.xml'])
     sitemap.handle_link('http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent')
-    assert not sitemap.sitemap_urls and sitemap.urls == ['http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent']
+    assert len(sitemap.sitemap_urls) == 1 and sitemap.urls == ['http://programtalk.com/java-api-usage-examples/org.apache.xml.security.stax.securityEvent.SecurityEvent']
 
     # similar domain names
     assert not is_similar_domain('kleins-weindepot.de', 'eurosoft.net')
@@ -54,17 +62,17 @@ def test_extraction():
     url = 'https://de.sitemaps.org/1'
     sitemap_url = 'https://de.sitemaps.org/sitemap.xml'
     domain, baseurl = get_hostinfo(sitemap_url)
-    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [])
     sitemap.handle_link(url)
     assert not sitemap.sitemap_urls and sitemap.urls == [url]
 
     # diverging domains
     url = 'https://www.software.info/1'
-    sitemap_url = 'https://example.org/sitemap.xml'
-    domain, baseurl = get_hostinfo(sitemap_url)
-    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap_urls = ['https://example.org/sitemap.xml']
+    domain, baseurl = get_hostinfo(sitemap_urls[0])
+    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_urls)
     sitemap.handle_link(url)
-    assert not sitemap.sitemap_urls and not sitemap.urls
+    assert len(sitemap.sitemap_urls) == 1 and not sitemap.urls
 
     # don't take this one?
     #url = 'https://subdomain.sitemaps.org/1'
@@ -78,7 +86,7 @@ def test_extraction():
     assert sitemaps.is_plausible_sitemap('http://test.org/sitemap.xml', '<!DOCTYPE html><html><body/></html>') is False
     assert sitemaps.is_plausible_sitemap('http://test.org/sitemap', '<!DOCTYPE html><html><body/></html>') is False
     # invalid
-    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [])
     sitemap.content = '<html>\n</html>'
     sitemap.extract_sitemap_links()
     assert not sitemap.sitemap_urls and not sitemap.urls
@@ -89,7 +97,7 @@ def test_extraction():
     with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
     assert sitemaps.is_plausible_sitemap('http://sitemaps.org/sitemap.xml', teststring) is True
-    sitemap = sitemaps.SitemapObject(baseurl, domain, sitemap_url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [])
     sitemap.content = teststring
     sitemap.extract_sitemap_links()
     assert not sitemap.sitemap_urls and len(sitemap.urls) == 84
@@ -103,20 +111,20 @@ def test_extraction():
     filepath = os.path.join(RESOURCES_DIR, 'sitemap2.xml')
     with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
-    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [url])
     sitemap.content = teststring
     sitemap.extract_sitemap_links()
-    assert sitemap.sitemap_urls == ['http://www.example.com/sitemap1.xml.gz', 'http://www.example.com/sitemap2.xml.gz'] and not sitemap.urls
+    assert sitemap.sitemap_urls == ['http://www.example.com/sitemap.xml', 'http://www.example.com/sitemap1.xml.gz', 'http://www.example.com/sitemap2.xml.gz'] and not sitemap.urls
 
     # hreflang
-    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap', 'en')
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', [], 'en')
     sitemap.content = '<?xml version="1.0" encoding="UTF-8"?><urlset><url><loc>http://www.test.org/english/page.html</loc></url></urlset>'
     sitemap.process()
     assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['http://www.test.org/english/page.html'])
     filepath = os.path.join(RESOURCES_DIR, 'sitemap-hreflang.xml')
     with open(filepath, "r", encoding="utf-8") as f:
         teststring = f.read()
-    sitemap = sitemaps.SitemapObject(baseurl, domain, url, 'de')
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [], 'de')
     sitemap.content = teststring
     sitemap.extract_sitemap_langlinks()
     assert sitemap.sitemap_urls == ['http://www.example.com/sitemap-de.xml.gz']
@@ -127,12 +135,12 @@ def test_extraction():
     filepath = os.path.join(RESOURCES_DIR, 'sitemap.xml.gz')
     with open(filepath, 'rb') as f:
         teststring = f.read()
-    teststring = decode_response(teststring)
+    teststring = decode_file(teststring)
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap.xml.gz', teststring) is True
-    sitemap = sitemaps.SitemapObject(baseurl, domain, url)
+    sitemap = sitemaps.SitemapObject(baseurl, domain, [url])
     sitemap.content = teststring
     sitemap.extract_sitemap_links()
-    assert len(sitemap.sitemap_urls) == 0 and len(sitemap.urls) == 84
+    assert len(sitemap.sitemap_urls) == 1 and len(sitemap.urls) == 84
 
     # check contents
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap.xml.gz?value=1', teststring) is True
@@ -140,13 +148,13 @@ def test_extraction():
     # TXT links
     content = 'Tralala\nhttps://test.org/1\nhttps://test.org/2'
     assert sitemaps.is_plausible_sitemap('http://example.org/sitemap', content) is True
-    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap')
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', [])
     sitemap.content = 'Tralala\nhttps://test.org/1\nhttps://test.org/2'
     sitemap.process()
     assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['https://test.org/1', 'https://test.org/2'])
 
     # TXT links + language
-    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', 'https://test.org/sitemap', 'en')
+    sitemap = sitemaps.SitemapObject('https://test.org/', 'test.org', [], 'en')
     sitemap.content = 'Tralala\nhttps://test.org/en/1\nhttps://test.org/en/2\nhttps://test.org/es/3'
     sitemap.process()
     assert (sitemap.sitemap_urls, sitemap.urls) == ([], ['https://test.org/en/1', 'https://test.org/en/2'])
@@ -164,6 +172,7 @@ def test_robotstxt():
 
 def test_whole():
     "Test whole process."
+    trafilatura.settings.MAX_SITEMAPS_SEEN = 1
     results = sitemaps.sitemap_search("https://www.sitemaps.org", target_lang="de")
     assert len(results) == 8
 
