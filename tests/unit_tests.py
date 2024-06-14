d2bcb1cae5a3ed1f3191168cf39decd7b61c99a4
Adrien Barbaresi <adbar@users.noreply.github.com>
2024-06-14 16:59:10 +0200

    baseline: review extractors, JSON parsing, and cleaning (#619)
    * baseline: review extractor sequence, JSON parsing, and cleaning
    
    * simplify
    
    * add explicit baseline tests
    
    * add types

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -21,8 +21,7 @@ except ImportError:
     from charset_normalizer import detect
 
 import trafilatura.htmlprocessing
-from trafilatura import (bare_extraction, baseline, extract, html2txt,
-                         process_record, xml)
+from trafilatura import bare_extraction, extract, process_record, xml
 from trafilatura.core import Extractor
 from trafilatura.external import sanitize_tree, try_justext
 from trafilatura.main_extractor import (handle_formatting, handle_image,
@@ -404,37 +403,6 @@ trafilatura.extract("")
     my_document = html.fromstring('<html><body><article><h4 id="1theinoperator">1) The <code>in</code> Operator</h4><p>The easiest way to check if a Python string contains a substring is to use the <code>in</code> operator. The <code>in</code> operator is used to check data structures for membership in Python. It returns a Boolean (either <code>True</code> or <code>False</code>) and can be used as follows:</p></article></body></html>')
     my_result = extract(my_document, output_format='xml', no_fallback=True, include_formatting=True, config=ZERO_CONFIG)
     assert '<head rend="h4">1) The <code>in</code> Operator</head>' in my_result and '<p>The easiest way to check if a Python string contains a substring is to use the <code>in</code> operator. The <code>in</code> operator is used to check data structures for membership in Python. It returns a Boolean (either <code>True</code> or <code>False</code>) and can be used as follows:</p>' in my_result
-
-
-def test_baseline():
-    _, string, length = baseline('')
-    assert (string, length) == ('', 0)
-    my_document = r'<html><body><script type="application/ld+json">{"description":"In letzter Zeit kam man am Begriff \"Hygge\", was so viel wie \"angenehm\" oder \"gemütlich\" bedeutet, ja nicht vorbei. Jetzt macht ihm ein neuer Glücks-Trend ...","image":[{"name":"Mit der Ikigai-Methode wirst du glücklicher","url":"https:\/\/image.brigitte.de\/10973004\/uncropped-0-0\/7d00b2658fd0a3b19e1b161f4657cc20\/Xw\/ikigai--1-.jpg","width":"2048","height":"1366","@type":"ImageObject"},{"name":"Mit der Ikigai-Methode wirst du glücklicher","url":"https:\/\/image.brigitte.de\/10973004\/16x9-1280-720\/bf947c7c24167d7c0adae0be10942d57\/Uf\/ikigai--1-.jpg","width":"1280","height":"720","@type":"ImageObject"},{"name":"Mit der Ikigai-Methode wirst du glücklicher","url":"https:\/\/image.brigitte.de\/10973004\/16x9-938-528\/bf947c7c24167d7c0adae0be10942d57\/JK\/ikigai--1-.jpg","width":"938","height":"528","@type":"ImageObject"},{"name":"Mit der Ikigai-Methode wirst du glücklicher","url":"https:\/\/image.brigitte.de\/10973004\/large1x1-622-622\/f5544b7d67e1be04f7729b130e7e0485\/KN\/ikigai--1-.jpg","width":"622","height":"622","@type":"ImageObject"}],"mainEntityOfPage":{"@id":"https:\/\/www.brigitte.de\/liebe\/persoenlichkeit\/ikigai-macht-dich-sofort-gluecklicher--10972896.html","@type":"WebPage"},"headline":"Ikigai macht dich sofort glücklicher!","datePublished":"2019-06-19T14:29:08+0000","dateModified":"2019-06-19T14:29:10+0000","author":{"name":"BRIGITTE.de","@type":"Organization"},"publisher":{"name":"BRIGITTE.de","logo":{"url":"https:\/\/image.brigitte.de\/11476842\/uncropped-0-0\/f19537e97b9189bf0f25ce924168bedb\/kK\/bri-logo-schema-org.png","width":"167","height":"60","@type":"ImageObject"},"@type":"Organization"},"articleBody":"In letzter Zeit kam man am Begriff \"Hygge\" (\"gemütlich\" oder \"angenehm\") nicht vorbei. Jetzt macht ihm ein neuer Glücks-Trend Konkurrenz: \"Ikigai\". Bist du glücklich? Schwierige Frage, nicht wahr? Viele von uns müssen da erst mal überlegen.","@type":"NewsArticle"}</script></body></html>'
-    _, result, _  = baseline(my_document)
-    assert result.startswith('In letzter Zeit kam man') and result.endswith('erst mal überlegen.')
-    my_document = '<html><body><article>' + 'The article consists of this text.'*10 + '</article></body></html>'
-    _, result, _ = baseline(my_document)
-    assert result is not None
-    my_document = '<html><body><article><b>The article consists of this text.</b></article></body></html>'
-    _, result, _ = baseline(my_document)
-    assert result is not None
-    my_document = '<html><body><quote>This is only a quote but it is better than nothing.</quote></body></html>'
-    _, result, _ = baseline(my_document)
-    assert result is not None
-    my_document = "<html><body><div>   Document body...   </div><script> console.log('Hello world') </script></body></html>"
-    _, result, _ = baseline(my_document)
-    assert result == 'Document body...'
-
-
-def test_html2txt():
-    mydoc = "<html><body>Here is the body text</body></html>"
-    assert html2txt(mydoc) == "Here is the body text"
-    assert html2txt(html.fromstring(mydoc)) == "Here is the body text"
-    assert html2txt("") == ""
-    assert html2txt("123") == ""
-    assert html2txt("<html></html>") == ""
-    assert html2txt("<html><body/></html>") == ""
-    assert html2txt("<html><body><style>font-size: 8pt</style><p>ABC</p></body></html>") == "ABC"
 
 
 def test_external():
@@ -1415,7 +1383,7 @@ def test_is_probably_readerable():
     assert is_probably_readerable(very_large_doc, options)
     assert called
 
-    # should use default node visibility checker 
+    # should use default node visibility checker
     assert is_probably_readerable(visible_doc)
     assert not is_probably_readerable(invisible_doc)
 
@@ -1497,7 +1465,6 @@ if __name__ == '__main__':
     test_htmlprocessing()
     test_extraction_options()
     test_precision_recall()
-    test_baseline()
     test_xmltocsv()
     test_tojson()
     test_python_output()
--- trafilatura/baseline.py
+++ trafilatura/baseline.py
@@ -1,24 +1,28 @@
+"""
+Module regrouping baseline and basic extraction functions.
+"""
 # pylint:disable-msg=E0611
-import re
 
-from lxml.etree import Element, SubElement
+import json
 
+from typing import Any, Tuple
+
+from lxml.etree import _Element, Element, SubElement
+
+from .htmlprocessing import delete_element
 from .settings import BASIC_CLEAN_XPATH
 from .utils import load_html, trim
 
 
-JSON_SEARCH = re.compile(r'"articlebody": *"(.+?)(?<!\\)"', re.I)
 
-
-
-def basic_cleaning(tree):
+def basic_cleaning(tree: _Element) -> _Element:
     "Remove a few section types from the document."
     for elem in BASIC_CLEAN_XPATH(tree):
-        elem.getparent().remove(elem)
+        delete_element(elem)
     return tree
 
 
-def baseline(filecontent):
+def baseline(filecontent: Any) -> Tuple[_Element, str, int]:
     """Use baseline extraction function targeting text paragraphs and/or JSON metadata.
 
     Args:
@@ -33,59 +37,76 @@ def baseline(filecontent):
     postbody = Element('body')
     if tree is None:
         return postbody, '', 0
+
     # scrape from json text
+    temp_text = ""
     for elem in tree.iterfind('.//script[@type="application/ld+json"]'):
-        if elem.text and '"article' in elem.text:
-            mymatch = JSON_SEARCH.search(elem.text)
-            if mymatch:
+        if elem.text and 'articleBody' in elem.text:
+            try:
+                json_body = json.loads(elem.text).get("articleBody")
+            except Exception:  # JSONDecodeError or 'list' object has no attribute 'get'
+                json_body = ""
+            if json_body:
                 elem = SubElement(postbody, 'p')
-                elem.text = trim(mymatch[1].replace('\\"', '"'))
-                return postbody, elem.text, len(elem.text)
+                elem.text = trim(load_html(json_body).text_content()) if "<p>" in json_body else trim(json_body)
+                temp_text += " " + json_body
+                # return postbody, elem.text, len(elem.text)
+    temp_text = temp_text.strip()
+    if len(temp_text) > 100:
+        return postbody, temp_text, len(temp_text)
 
     tree = basic_cleaning(tree)
 
     # scrape from article tag
-    article_elem = tree.find('.//article')
-    if article_elem is not None:
-        temp_text = trim(article_elem.text_content())
-        if len(temp_text) > 100:
+    temp_text = ""
+    for article_elem in tree.iterfind('.//article'):
+        text = trim(article_elem.text_content())
+        if len(text) > 100:
             elem = SubElement(postbody, 'p')
-            elem.text = temp_text
-            return postbody, temp_text, len(temp_text)
+            elem.text = text
+            temp_text += " " + text
+    if len(postbody) > 0:
+        temp_text = temp_text.strip()
+        # temp_text = trim('\n'.join(postbody.itertext()))
+        return postbody, temp_text, len(temp_text)
+
     # scrape from text paragraphs
     results = set()
+    temp_text = ""
+    # postbody = Element('body')
     for element in tree.iter('blockquote', 'code', 'p', 'pre', 'q', 'quote'):
-        entry = element.text_content()
+        entry = trim(element.text_content())
         if entry not in results:
             elem = SubElement(postbody, 'p')
             elem.text = entry
+            temp_text += " " + entry
             results.add(entry)
-    temp_text = trim('\n'.join(postbody.itertext()))
+    # temp_text = trim('\n'.join(postbody.itertext()))
+    temp_text = temp_text.strip()
     if len(temp_text) > 100:
         return postbody, temp_text, len(temp_text)
+
     # default strategy: clean the tree and take everything
     postbody = Element('body')
     body_elem = tree.find('.//body')
     if body_elem is not None:
-        # elem.text = trim(body_elem.text_content())
-        text = '\n'.join([trim(e) for e in body_elem.itertext()])
-        if len(text) > 100:
-            elem = SubElement(postbody, 'p')
-            elem.text = text
-            return postbody, text, len(text)
+        elem = SubElement(postbody, 'p')
+        # todo: sanitize?
+        elem.text = '\n'.join([trim(e) for e in body_elem.itertext()])
+        return postbody, elem.text, len(elem.text)
+
     # new fallback
-    text = html2txt(tree)
     elem = SubElement(postbody, 'p')
-    elem.text = text
-    return postbody, text, len(text)
-    # old: return postbody, '', 0
+    elem.text = html2txt(tree, clean=False)
+    return postbody, elem.text, len(elem.text)
 
 
-def html2txt(content):
+def html2txt(content: Any, clean: bool = True) -> str:
     """Run basic html2txt on a document.
 
     Args:
         content: HTML document as string or LXML element.
+        clean: remove potentially undesirable elements.
 
     Returns:
         The extracted text in the form of a string or an empty string.
@@ -97,5 +118,6 @@ def html2txt(content):
     body = tree.find(".//body")
     if body is None:
         return ""
-    tree = basic_cleaning(tree)
+    if clean:
+        body = basic_cleaning(body)
     return " ".join(body.text_content().split()).strip()
