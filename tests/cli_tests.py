786dacc9f6512308ecae2eb64166a897f8e35c46
Adrien Barbaresi <barbaresi@bbaw.de>
2024-10-07 17:28:08 +0200

    revamp STDIN test

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -151,8 +151,11 @@ def test_climain(capfd):
     # help display
     assert subprocess.run([trafilatura_bin, "--help"], check=True).returncode == 0
     # piped input
-    empty_input = b"<html><body></body></html>"
-    assert subprocess.run([trafilatura_bin], input=empty_input, check=True).returncode == 0
+    empty_input = b"<html><body><article>" + b"<p>ABC</p>"*100 + b"</article></body></html>"
+    result = subprocess.run([trafilatura_bin], input=empty_input, check=True)
+    assert result.returncode == 0
+    captured = capfd.readouterr()
+    assert captured.out.strip().endswith("ABC")
     # input directory walking and processing
     env = os.environ.copy()
     if os.name == "nt":
@@ -288,15 +291,6 @@ def test_download():
 # @patch('trafilatura.settings.MAX_FILES_PER_DIRECTORY', 1)
 def test_cli_pipeline():
     """test command-line processing pipeline"""
-    # straight command-line input
-    # testargs = ['', '<html><body>Text</body></html>']
-    # with patch.object(sys, 'argv', testargs):
-    #    args = cli.parse_args(testargs)
-    # f = io.StringIO()
-    # with redirect_stdout(f):
-    #    cli.process_args(args)
-    # assert len(f.getvalue()) == 0
-
     # Force encoding to utf-8 for Windows in future processes spawned by multiprocessing.Pool
     os.environ["PYTHONIOENCODING"] = "utf-8"
 
