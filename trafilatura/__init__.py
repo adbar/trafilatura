d3804000c0c2c531fd83946e9f211f3dd283fcf4
Adrien Barbaresi <barbaresi@bbaw.de>
2024-11-07 18:55:58 +0100

    setup: explicit exports through __all__

--- trafilatura/__init__.py
+++ trafilatura/__init__.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
 """
 Python & command-line tool to gather text on the Web:
 web crawling/scraping, extraction of text, metadata, comments.
 """
 
-
-__title__ = 'trafilatura'
-__author__ = 'Adrien Barbaresi and contributors'
+__title__ = "Trafilatura"
+__author__ = "Adrien Barbaresi and contributors"
 __license__ = "Apache-2.0"
-__copyright__ = 'Copyright 2019-2024, Adrien Barbaresi'
-__version__ = '1.12.2'
+__copyright__ = "Copyright 2019-present, Adrien Barbaresi"
+__version__ = "1.12.2"
 
 
 import logging
@@ -21,3 +19,14 @@ from .metadata import extract_metadata
 from .utils import load_html
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+    "bare_extraction",
+    "baseline",
+    "extract",
+    "extract_metadata",
+    "fetch_response",
+    "fetch_url",
+    "html2txt",
+    "load_html",
+]
